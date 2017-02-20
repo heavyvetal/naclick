245a06f1454e92e9e1dab0790b33979eca13381b
heavyvetal <x_15@mail.ru>
2017-02-20 08:58:14 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,180 @@
+<?php get_header(); ?>
+
+<body>
+<!-- {literal} -->
+        <script type='text/javascript'>
+        window['li'+'veT'+'ex'] = true,
+        window['liv'+'e'+'T'+'exI'+'D'] = 125297,
+        window['live'+'Tex'+'_o'+'b'+'j'+'ect'] = true;
+        (function() {
+        var t = document['cr'+'eateE'+'lement']('script');
+        t.type ='text/javascript';
+        t.async = true;
+        t.src = '//cs15'+'.'+'li'+'vet'+'ex'+'.ru'+'/js/cli'+'ent.js';
+        var c = document['g'+'etElements'+'ByTagNa'+'m'+'e']('script')[0];
+        if ( c ) c['paren'+'tNode']['ins'+'e'+'r'+'tBefor'+'e'](t, c);
+        else document['docu'+'m'+'e'+'nt'+'El'+'ement']['f'+'i'+'rstC'+'hild']['appe'+'n'+'dChil'+'d'](t);
+        })();
+        </script>
+        <!-- {/literal} -->
+<script>
+var template_path = "<?php echo get_template_directory_uri();?>";
+</script>
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-KK8SPC"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KK8SPC');</script>
+<!-- End Google Tag Manager -->
+<div id="section1">
+	<header class="main-header">
+		<a href="<?php echo get_site_url();?>"><img id="logo" src="<?php echo get_template_directory_uri();?>/images/logo.png" alt="Logo"></a>
+			<div id="phones">
+			<p>(050) 805 08 04</p>
+			<p>(067) 637 28 48</p>
+			<p>(093) 907 57 55</p>
+		</div>
+	</header>
+	<div id="top">	
+		<div id="top1">
+			<p class="small">Больше, чем</p>
+			<p class="large">контекстная реклама</p>
+		</div>
+		<div id="top2">
+			<ul class="list-inline">
+				<li><img src="<?php echo get_template_directory_uri();?>/images/pic1.png" alt="pic1">Анализируем ваш бизнес и подбираем рекламные системы, которые дадут наибольшую отдачу</li>
+				<li><img src="<?php echo get_template_directory_uri();?>/images/pic1.png" alt="pic1">Сфокусированы на увеличении продаж и снижении стоимости привлечения одного клиента</li>
+				<li><img src="<?php echo get_template_directory_uri();?>/images/pic1.png" alt="pic1">Непрерывно оптимизируем рекламную кампанию и проводим A/B-тесты</li>
+			</ul>
+		</div>
+		<div id="top3">
+			<a class="button btn1" href="javascript:void(0);">Оставить заявку</a>	
+		</div>
+	</div>
+</div>
+<div id="section2">
+	<div class="part1">
+		<p>Настройка рекламных кампаний и систем аналитики</p> 
+		<p>с полной прозрачностью окупаемости рекламных вложений</p>
+	</div>
+	<div class="part2">
+		<div class="wrapper">
+			<table id="tab1">
+				<tr>
+					<td><div class="first"></div><div class="second"><img class="img1" src="<?php echo get_template_directory_uri();?>/images/sec2-circle.png" alt="circle"></div><div class="clear"></div></td>
+					<td><img src="<?php echo get_template_directory_uri();?>/images/sec2-circle.png" alt="circle"></td>
+					<td><img src="<?php echo get_template_directory_uri();?>/images/sec2-circle.png" alt="circle"></td>
+					<td><div class="first"><img class="img4" src="<?php echo get_template_directory_uri();?>/images/sec2-circle.png" alt="circle"></div><div class="second"></div><div class="clear"></div></td>
+				</tr>
+				<tr>
+					<td>Создание рекламных кампаний</td>
+					<td>Контроль за расходом бюджета</td>
+					<td>Оптимизация под заданные цели</td>
+					<td>Составление подробной отчетности</td>
+				</tr>
+			</table>
+			<table id="tab2">
+				<tr><td><img class="img4" src="<?php echo get_template_directory_uri();?>/images/sec2-circle.png" alt="circle">Создание рекламных кампаний</td></tr>
+				<tr><td><img class="img4" src="<?php echo get_template_directory_uri();?>/images/sec2-circle.png" alt="circle">Контроль расхода бюджета</td></tr>
+				<tr><td><img class="img4" src="<?php echo get_template_directory_uri();?>/images/sec2-circle.png" alt="circle">Оптимизация под заданные KPI</td></tr>
+				<tr><td><img class="img4" src="<?php echo get_template_directory_uri();?>/images/sec2-circle.png" alt="circle">Формирование отчетности</td></tr>	
+			</table>
+		</div>
+	</div>
+</div>
+<div id="section3">
+	<div id="parter">
+	<div class="part1">
+		<p>У нас есть все необходимые</p> 
+		<p>вашему бизнесу инструменты</p>
+	</div>
+	<div class="part2">
+		<div class="wrapper">
+			<div class="left">
+				<img src="<?php echo get_template_directory_uri();?>/images/logo.png" alt="logo">
+				<p>
+				В своей работе опираемся на мощные технологические решения - платформы управления контекстной рекламой	
+				</p>
+			</div>
+			<div class="right">
+				<ul>
+					<li>Проработка структуры кампании</li>
+					<li>Работа с тысячами ключевых слов</li>
+					<li>Настройка целей в Google Analytics и Яндекс. Метрике</li>
+					<li>Внедрение систем сквозной аналитики</li>
+					<li>Разработка посадочных страниц с учетом особенностей каждого бизнеса</li>
+				</ul>
+			</div>
+			<div class="clear"></div>
+		</div>
+	</div>
+	</div>
+	<div class="part3">
+		<div>
+			<p>Узнайте, как мы можем помочь вашему бизнесу</p>
+			<p>Оставьте ваше имя и номер телефона. Мы перезвоним и все расскажем.</p>
+		</div>
+		<div>
+			<form id="form3">
+				<input class="inp-txt required" type="text" name="Имя" placeholder="Ваше имя">
+				<input class="inp-txt required" type="text" name="Телефон" placeholder="Номер телефона">
+			</form>
+		</div>
+		<div>
+			<a href="javascript:void(0);" class="button btn3">Жду звонка</a>
+		</div>
+	</div>
+</div>
+<div id="section4">
+	<div class="wrapper">
+		<p class="large">
+		О нашем опыте в блоге
+		</p>
+		<p class="small">
+		Иногда мы публикуем наши наблюдения и инсайды в популярных интернет-изданиях. Посмотрите, о чем мы пишем.
+		</p>
+		<div class="btn-blog">
+			<a href="<?php echo get_site_url();?>/blog">Блог naClick</a>
+		</div>
+	<hr>	
+	</div>
+</div>
+<footer>
+	<div class="wrapper">
+		<div class="left"><img src="<?php echo get_template_directory_uri();?>/images/skype.png" alt="">igornazim</div>
+		<div class="center"><img src="<?php echo get_template_directory_uri();?>/images/mail.png" alt="">igor.nazim@naclick.com.ua</div>
+		<div class="right"><img src="<?php echo get_template_directory_uri();?>/images/phone.png" alt="">050 805 08 04</div>
+		<div class="clear"></div>
+	</div>
+</footer>
+<div id="overlay"></div>
+<div id="popup">
+	<form id="form1">
+		<table id="table1">
+			<tr>
+				<td><input class="inp-txt required" type="text" name="Имя" placeholder="Ваше имя"></td>
+				<td><input class="inp-txt required" type="text" name="Телефон" placeholder="Ваш телефон"></td>
+			</tr>
+			<tr>
+				<td><input class="inp-txt" type="text" name="E-mail" placeholder="Ваш e-mail"></td>
+				<td><input class="inp-txt" type="text" name="Сайт" placeholder="Ваш сайт"></td>
+			</tr>
+			<tr><td colspan="2"><a class="button btn2" href="javascript:void(0);">Отправить заявку</a></td></tr>
+		</table>
+	</form>
+	<form id="form2">
+		<table id="table2">
+			<tr><td><input class="inp-txt required" type="text" name="Имя" placeholder="Ваше имя"></td></tr>
+			<tr><td><input class="inp-txt required" type="text" name="Телефон" placeholder="Ваш телефон"></td></tr>
+			<tr><td><input class="inp-txt" type="text" name="E-mail" placeholder="Ваш e-mail"></td></tr>
+			<tr><td><input class="inp-txt" type="text" name="Сайт" placeholder="Ваш сайт"></td></tr>
+			<tr><td><a class="button btn2" href="javascript:void(0);">Отправить заявку</a></td></tr>
+		</table>
+	</form>
+	<div id="close"></div>
+	<div id="thanks">Спасибо за заказ, мы свяжемся с Вами в ближайшее время.</div>
+</div>
+<?php get_footer(); ?>
