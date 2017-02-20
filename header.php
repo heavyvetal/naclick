8d926da63b52947eac93c2c5732af9a5279c16e4
heavyvetal <x_15@mail.ru>
2017-02-20 09:00:06 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title><?php bloginfo('name'); ?></title>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!--meta name="description" content="Настройка рекламных кампаний и систем аналитики с полной прозрачностью окупаемости рекламных вложений"-->
+<link rel="shortcut icon" href="<?php echo get_template_directory_uri();?>/images/favicon.ico" type="image/x-icon">
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>">
+<script type="text/javascript" src="<?php echo get_template_directory_uri();?>/js/jquery.min.js"></script>
+<script type="text/javascript" src="<?php echo get_template_directory_uri();?>/js/naClick.js"></script>
+<?php wp_head(); ?>
+</head>
